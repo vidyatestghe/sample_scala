cd6e08ce8eb5b8294204cfbb157b097a38d4ec5e
sharathm <sharathm@qhode.com>
2013-10-16 13:33:51 +0530

    sample source for scala with sbt build definition

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "scala"
+
+version := "1.0"
+
+scalaVersion := "2.10.2"
---
+++ src/main/scala/Hello.scala
@@ -0,0 +1,13 @@
+object HelloWorld {
+   def main(args: Array[String]) {
+      println("Hello, world!")
+      println(square(10))
+      println(multiple(5,4))
+   }
+   def square(x:Int): Int= {
+	x*x
+   }
+   def multiple(x:Int,y:Int): Int= {
+	x*y
+   }
+}
